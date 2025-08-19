65cb052c661796bd68bc031049995e251d02a3c9
incmsx <kondratovich2431@gmail.com>
2025-08-19 22:52:15 +0300

    burn + refund

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,36 +1,64 @@
 // SPDX-License-Identifier:  MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import {IERC721Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
 
-contract NFT is ERC721, Ownable{
+contract NFT is ERC721Upgradeable,ERC721BurnableUpgradeable, OwnableUpgradeable, UUPSUpgradeable{
 
-    uint256 public BASIC_COST = 0.0001 ether;
+    event Refund(address indexed to, uint256 indexed tokenId, uint256 amount);
 
-    uint256 private tokensIdCounter = 0; 
-
-    string public constant BASE_URI = "ipfs://bafkreiehkbbvbl2bb7rgu7zevqdrtx6rkb4pxhalxtwxkiphiatvpggxmi";
-
-    constructor()ERC721("ShitNFT", "SHIT")Ownable(_msgSender()) { 
-    }
-
-    // Indicates that user don't have enough money to mint NFT
-    error NotEnoughMoney(uint256 sendedAmount, uint256 nftCost); 
-
+    error NotEnoughMoney(uint256 sentAmount, uint256 nftCost); 
     error TransferError(address to, uint256 amount);
 
+    uint256 private basicCost;
+    uint256 private tokenIdCounter; 
+    string private constant BASE_URI = "ipfs://bafkreiehkbbvbl2bb7rgu7zevqdrtx6rkb4pxhalxtwxkiphiatvpggxmi";
+    mapping (uint256 => uint256) price;
+
+    modifier onlyMinter(uint256 tokenId){
+        require(ownerOf(tokenId) == _msgSender(), ERC721InvalidOwner(ownerOf(tokenId)));
+        _;
+    }
+    
+
+    function initializate(
+        string memory _name, 
+        string memory _symbol, 
+        uint256 _basicCost
+        ) 
+        initializer public
+    {
+        __ERC721_init(_name, _symbol);
+        __Ownable_init(_msgSender());
+        __ERC721Burnable_init();
+        __UUPSUpgradeable_init();
+
+        basicCost = _basicCost;
+        tokenIdCounter = 1;
+    }
+
     function mint() external payable{
-        if(msg.value != BASIC_COST) {
-            revert NotEnoughMoney(msg.value, BASIC_COST);
+        if(msg.value < basicCost) {
+            revert NotEnoughMoney(msg.value, basicCost);
         }
 
-        _mint(_msgSender(), tokensIdCounter);
-        tokensIdCounter++;
+        _mint(_msgSender(), tokenIdCounter);
+        price[tokenIdCounter] = basicCost;
+        tokenIdCounter++;
+    }
+
+    function burn(uint256 tokenId) public override {
+        super.burn(tokenId);
+        _burnRefund(_msgSender(), price[tokenId], tokenId);
     }
 
     function changeBasicCost(uint256 newCost) external onlyOwner {
-        BASIC_COST = newCost;
+        basicCost = newCost;
     }
 
     function withdraw() external onlyOwner{
@@ -40,14 +68,26 @@ contract NFT is ERC721, Ownable{
         require(success, TransferError(owner(), totalEther));
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return BASE_URI;
-    }
-
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         _requireOwned(tokenId);
 
         string memory baseURI = _baseURI();
         return bytes(baseURI).length > 0 ? baseURI : "";
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return BASE_URI;
+    }
+
+    function _burnRefund(address minterAddress, uint256 nftPrice, uint256 tokenId) internal onlyMinter(tokenId){
+        uint256 refund = nftPrice * 9 / 10;
+        (bool success, ) = payable(minterAddress).call{value: refund}("");
+        require(success, TransferError(minterAddress, refund));
+
+        emit Refund(minterAddress, tokenId, nftPrice);
+    }
+    
+    function _authorizeUpgrade(address newImplementation) internal onlyOwner override {
+
     }
 }
